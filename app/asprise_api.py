3feda6623b7610ff0589713bce80dcb0edeffa54
Elga J Henry <henryelga2004@gmail.com>
2025-10-16 15:33:33 +0100

    Added .env config and .gitignore

--- app/asprise_api.py
+++ app/asprise_api.py
@@ -1,6 +1,11 @@
 import requests
+from dotenv import load_dotenv
+import os
 
-ASPRISE_API_URL = "https://ocr.asprise.com/api/v1/receipt"
+load_dotenv()
+
+ASPRISE_API_URL = os.getenv("ASPRISE_API_URL")
+ASPRISE_CLIENT_ID = os.getenv("ASPRISE_CLIENT_ID")
 
 def send_receipt_to_asprise(image_bytes: bytes, filename: str) -> dict:
     """
@@ -13,9 +18,9 @@ def send_receipt_to_asprise(image_bytes: bytes, filename: str) -> dict:
 
     # Data payload
     data = {
-        "client_id": "TEST",        # Using "TEST" for testing purposes 
-        "recognizer": "auto",       # Using "auto" for automatic recognition
-        "ref_no": "receipt-ocr",    # Reference number for tracking
+        "client_id": ASPRISE_CLIENT_ID,   # Using "TEST" for testing purposes 
+        "recognizer": "auto",           # Using "auto" for automatic recognition
+        "ref_no": "receipt-ocr",        # Reference number for tracking
     }
 
     response = requests.post(ASPRISE_API_URL, files=files, data=data)
