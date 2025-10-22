18e7e1e51f45044b172d27b90c5f79f4b27f0395
Yee Chean Chang <145558346+LeafMonarch@users.noreply.github.com>
2025-10-22 21:48:31 +0100

    style(22994): Styled LandingPage.tsx (#4)
    * Styled Landing Page
    
    * Removed important rule, and created Custom React Google Button
    
    * Added Google OAuth time flexibility
    
    * Implement a new Google OAuth time flexibility
    
    * Updated landingPage browser title and deeper text font color

---
+++ Frontend/src/components/CustomGoogleButton.tsx
@@ -0,0 +1,50 @@
+import { useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
+import { API_ROUTES, GOOGLE_CONSTS } from "../config/consts";
+import { useNavigate } from "react-router-dom";
+import "./customGoogleButton.css";
+
+/**
+ * Custom Google Login Button Component
+ * 
+ * Instead of using the default Google-rendered button,
+ * this allows full styling control (consistent with our Figma design).
+ * 
+ * @returns JSX.Element
+ */
+export const CustomGoogleButton: React.FC = () => {
+  const navigate = useNavigate();
+
+  // Initialize Google Login
+  const login = useGoogleLogin({
+    // Obtain temporary authorization code from Google for backend exchange
+    flow: "auth-code",
+    onSuccess: async (tokenResponse) => {
+      try {
+        // Send the authorization code to FastAPI backend for token exchange
+        const res = await axios.post(API_ROUTES.VERIFY_GOOGLE, {
+          token: tokenResponse.code,
+        });
+
+        localStorage.setItem("user", JSON.stringify(res.data.user));
+
+        navigate("/dashboard");
+      } catch (err) {
+        console.error("Login failed:", err);
+      }
+    },
+    onError: () => console.error("Login Failed"),
+  });
+
+  return (
+    <button className="custom__google-btn" onClick={() => login()}>
+      <img
+        src= {GOOGLE_CONSTS.GOOGLE_IMAGE_URL}
+        alt= {GOOGLE_CONSTS.GOOGLE_ALT_TEXT}
+        width= {GOOGLE_CONSTS.GOOGLE_ICON_SIZE}
+        height= {GOOGLE_CONSTS.GOOGLE_ICON_SIZE}
+      />
+      {GOOGLE_CONSTS.GOOGLE_TEXT}
+    </button>
+  );
+};
--- Frontend/src/config/consts.ts
+++ Frontend/src/config/consts.ts
@@ -6,3 +6,11 @@ export const API_ROUTES = {
   UPLOAD_RECEIPT: `${API_BASE_URL}/upload-receipt`,
   VERIFY_GOOGLE: `${API_BASE_URL}/auth/google`,
 };
+
+// Google OAuth related constants
+export const GOOGLE_CONSTS = {
+  GOOGLE_IMAGE_URL: "https://developers.google.com/identity/images/g-logo.png",
+  GOOGLE_TEXT: "Continue with Google",
+  GOOGLE_ALT_TEXT: "Google logo", 
+  GOOGLE_ICON_SIZE: 18,
+}
