6e0024aec51b58f0e0afc95ff5ffb03a390c1893
Yee Chean Chang <145558346+LeafMonarch@users.noreply.github.com>
2025-11-23 18:16:12 +0000

    feat(63486): Implemented notification (#23)

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,18 +1,18 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { NotificationProvider } from "./components/Notification/NotificationContext";
 
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <GoogleOAuthProvider clientId={clientId}>
-      <BrowserRouter>
+  <GoogleOAuthProvider clientId={clientId}>
+    <BrowserRouter>
+      <NotificationProvider>
         <App />
-      </BrowserRouter>
-    </GoogleOAuthProvider>
-  </React.StrictMode>
+      </NotificationProvider>
+    </BrowserRouter>
+  </GoogleOAuthProvider>
 );
