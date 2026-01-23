8b7b25cafc32c4546e95d46d4033b842f7c09c3f
EricHanJf <jf08942378268@gmail.com>
2026-01-23 22:30:29 +0000

    fix(23834): restore missing onItemClick after merge and resolved css as well

--- Frontend/src/components/RecipeItemSection/RecipeItemSection.tsx
+++ Frontend/src/components/RecipeItemSection/RecipeItemSection.tsx
@@ -27,7 +27,7 @@ interface RecipeItemSectionProps {
  * @param param0 
  * @returns 
  */
-const RecipeItemSection: React.FC<RecipeItemSectionProps> = ({ title, seeMore = true, onSeeMore, items, emptyTitle, emptySubtitle, emptyImage }) => {
+const RecipeItemSection: React.FC<RecipeItemSectionProps> = ({ title, seeMore = true, onSeeMore, items, onItemClick, emptyTitle, emptySubtitle, emptyImage }) => {
     return (
         <div className="recipeItemSection__container">
             <div className="recipeItemSection__header">
